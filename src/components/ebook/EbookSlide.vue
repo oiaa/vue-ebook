1dd0b0a9f8e72a93e687ca305527063b8ae18e85
chenrui <2035851466@qq.com>
2019-04-01 09:34:14 +0800

    Tab选项切换

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -4,8 +4,17 @@
             <transition name="slide-right">
                 <div class="content" v-if="settingVisible === 3">
                     <div class="content-page-wrapper">
-                        <div class="content-page"></div>
-                        <div class="content-page-tab"></div>
+                        <div class="content-page">
+                            <component :is="currentTab === 1 ? content : bookmark"></component>
+                        </div>
+                        <div class="content-page-tab">
+                            <div class="content-page-tab-item" :class="{'selected': currentTab === 1}" @click="selectTab(1)">
+                                {{ $t('book.navigation') }}
+                            </div>
+                            <div class="content-page-tab-item" :class="{'selected': currentTab === 2}" @click="selectTab(2)">
+                                {{ $t('book.bookmark') }}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </transition>
@@ -16,11 +25,21 @@
 
 <script>
 import { ebookMixin } from '../../utils/mixin.js';
+import EbookSlideContents from './EbookSlideContents.vue'
 
 export default {
     mixins: [ebookMixin],
     methods: {
-        
+        selectTab(tab) {
+            this.currentTab = tab;
+        }
+    },
+    data() {
+        return {
+            currentTab: 1,
+            content: EbookSlideContents,
+            bookmark: null
+        }
     },
 }
 </script>
@@ -43,13 +62,28 @@ export default {
         height: 100%;
 
         .content-page-wrapper {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            height: 100%;
 
             .content-page {
-
+                flex: 1;
+                width: 100%;
+                overflow: hidden;
             }
 
             .content-page-tab {
+                display: flex;
+                flex: 0 0 px2rem(48);
+                width: 100%;
+                height: px2rem(48);
 
+                .content-page-tab-item {
+                    flex: 1;
+                    font-size: px2rem(12);
+                    @include center;
+                }
             }
         }
     }
