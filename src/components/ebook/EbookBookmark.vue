33d4b110d71de2eb01c85355a54700dfd007c5ed
chenrui <2035851466@qq.com>
2019-04-04 16:39:16 +0800

    书签手势

--- src/components/ebook/EbookBookmark.vue
+++ src/components/ebook/EbookBookmark.vue
@@ -1,35 +1,132 @@
 <template>
-    <article class="ebook-bookmark">
+    <article class="ebook-bookmark" ref="bookmark">
         <section class="ebook-bookmark-text-wrapper">
-            <section class="ebook-bookmark-down-wrapper">
+            <section class="ebook-bookmark-down-wrapper" ref="iconDown">
                 <span class="icon-down"></span>
             </section>
             <div class="ebook-bookmark-text">
                 {{ text }}
             </div>
         </section>
-        <section class="ebook-bookmark-icon-wrapper">
-            <bookmark :color=" 'red' " :width="15" :height="35"></bookmark>
+        <section class="ebook-bookmark-icon-wrapper" :style=" isFixed ? fixedStyle : {} ">
+            <bookmark :color=" color " :width="15" :height="35" ref="bookmark"></bookmark>
         </section>
     </article>
 </template>
 
 <script>
 import Bookmark from '../common/Bookmark'
+import { realPx } from '../../utils/utils';
+import { ebookMixin } from '../../utils/mixin.js';
 const BLUE = '#346cbc'
 const WHITE = '#fff'
 export default {
+    mixins: [ebookMixin],
     components: {
         Bookmark
     },
+    computed: {
+        height() { //拖到多高算是到达临界点
+            return realPx(35)
+        },
+        threshold() { //临界值
+            return realPx(55)
+        },
+        fixedStyle() {
+            return {
+                position: 'fixed',
+                top: 0,
+                right: `${(window.innerWidth - this.$refs.bookmark.clientWidth) / 2}px`
+            }
+        }
+    },
     watch: {
         offsetY(v) {
-            
+            if (!this.bookAvailable || this.menuVisible || this.settingVisible >= 0) {
+                return
+            }
+
+            if(v >= this.height && v < this.threshold) { 
+                this.beforeThreshold(v);
+            } else if (v >= this.threshold) { 
+                this.afterThreshold(v);
+            } else if (v > 0 && v < this.height) { 
+                this.beforeHeight();
+            } else if (v === 0) { // 归位
+                this.restore()
+            }
         }
     },
     data() {
         return {
-            text: this.$t('book.pulldownAddMark')
+            text: '',
+            color: WHITE,
+            isFixed: false
+        }
+    },
+    methods: {
+        beforeHeight() {
+            // 状态1:未超过书签的高度
+            if (this.isBookmark) { // 当前页是否是书签页
+                this.text = this.$t('book.pulldownDeleteMark');
+                this.color = BLUE;
+                this.isFixed = true;
+            } else {
+                this.text = this.$t('book.pulldownAddMark');
+                this.color = WHITE;
+                this.isFixed = false;
+            }
+        },
+        beforeThreshold(v) {
+            // 状态2: 未到达临界状态
+            this.$refs.bookmark.style.top = `${-v}px`
+
+            this.beforeHeight()
+
+            const iconDown = this.$refs.iconDown
+            if (iconDown.style.transform === 'rotate(180deg)') {
+                iconDown.style.transform = `rotate(0deg)`
+            }
+            this.isFixed = false;
+        },
+        afterThreshold(v) {
+            // 状态3: 超越临界状态
+            this.$refs.bookmark.style.top = `${-v}px`
+
+            if (this.isBookmark) {
+                this.text = this.$t('book.releaseDeleteMark')
+                this.color = WHITE
+                this.isFixed = false;
+            } else {
+                this.text = this.$t('book.releaseAddMark')
+                this.color = BLUE
+                this.isFixed = true;
+            }
+
+            const iconDown = this.$refs.iconDown
+            if (iconDown.style.transform === '' || iconDown.style.transform === 'rotate(0deg)') {
+                iconDown.style.transform = `rotate(180deg)`
+            }
+        },
+        restore() {
+            // 状态4:归位
+            setTimeout(() => {
+                this.$refs.bookmark.style.top = `${-this.height}px`;
+                this.$refs.iconDown.style.transform = `rotate(0deg)`;
+            }, 200);
+            if (this.isFixed) {
+                this.setIsBookmark(true);
+                this.addBookmark();
+            } else {
+                this.setIsBookmark(false);
+                this.removeBookmark();
+            }
+        },
+        addBookmark() {
+
+        },
+        removeBookmark() {
+            
         }
     },
 }
@@ -39,12 +136,11 @@ export default {
 @import '../../assets/styles/global.scss';
 .ebook-bookmark {
     position: absolute;
-    top: px2rem(-350);
+    top: px2rem(-35);
     left: 0;
     z-index: 200;
     width: 100%;
-    height: px2rem(350);
-    background: pink;
+    height: px2rem(35);
 
     .ebook-bookmark-text-wrapper {
         position: absolute;
